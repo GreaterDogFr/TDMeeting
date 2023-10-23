3f51b9f1629b09c2ac06becad4204b0d7df6eecc
rabia <rabia_mennai@hotmail.fr>
2023-10-23 13:55:55 +0200

    creation objet personnage

---
+++ asset/js/style.js
@@ -0,0 +1,54 @@
+
+class personnage {
+
+    constructor(id, pseudo, photo, age, sexe, recherche, departement, description) {
+        this.id = id
+        this.pseudo = pseudo
+        this.photo = photo
+        this.age = age
+        this.sexe = sexe
+        this.recherche = recherche
+        this.departement = departement
+        this.description = description
+
+    }
+}
+
+let personne1 = new personnage('1', 'titi', 'TDMeeting\asset/img/man-1283231_1280.jpg', '30', 'Homme', 'Femme', '76', 'en temps voulut')
+let personne2 = new personnage('2', 'toto', 'TDMeeting\asset\img\man-3803551_1280.jpg', '40', 'Homme', 'Femme', '95', 'en temps voulut')
+let personne3 = new personnage('3', 'Sophie', 'TDMeeting\asset\img\pexels-andrea-piacquadio-774909.jpg', '37', 'Femme', 'Homme', '27', 'en temps voulut')
+let personne4 = new personnage('4', 'Fred', 'TDMeeting\asset\img\pexels-ba-tik-3754285.jpg', '29', 'Homme', 'Femme', '75', 'en temps voulut')
+let personne5 = new personnage('5', 'Sonia', 'TDMeeting\asset\img\pexels-daniel-xavier-1239291.jpg', '25', 'Femme', 'Homme', '60', 'en temps voulut')
+let personne6 = new personnage('6', 'doudou', 'TDMeeting\asset\img\pexels-jay-rane-943235.jpg', '30', 'homme', 'homme', '75',' en temps voulut' )
+let personne7 = new personnage('7', 'tender', 'TDMeeting\asset\img\pexels-pixabay-220453.jpg', '38', 'homme', 'homme', '75',' en temps voulut' )
+let personne8 = new personnage('8', 'chou', 'TDMeeting\asset\img\pexels-spencer-selover-428328.jpg', '30', 'homme', 'femme', '75',' en temps voulut' )
+let personne9 = new personnage('9', 'Lou', 'TDMeeting\asset\img\portrait-3204843_1280.jpg', '37', 'Femme', 'homme', '75',' en temps voulut' )
+let personne10 = new personnage('10', 'Celine', 'TDMeeting\asset\img\woman-1274056_1280.jpg', '32', 'Femme', 'homme', '75',' en temps voulut' )
+let personne11 = new personnage('11', 'sara', 'TDMeeting\asset\img\woman-1274361_1280.jpg', '24', 'Femme', 'homme', '75',' en temps voulut' )
+let personne12 = new personnage('12', 'doudou', 'TDMeeting\asset\img\woman-1919143_1280.jpg', '20', 'Femme', 'homme', '75',' en temps voulut' )
+let personne13 = new personnage('1', 'marion', 'TDMeeting\asset\img\adeleGomes.jpg', '30', 'Femme', 'Femme', '78', 'en temps voulut')
+let personne14 = new personnage('2', 'Amadou', 'TDMeeting\asset\img\amadou-Hassan.jpg', '31', 'Homme', 'Femme', '91', 'en temps voulut')
+let personne15 = new personnage('3', 'Clem', 'TDMeeting\asset\img\clementBarbosa.jpg', '34', 'Homme', 'Homme', '27', 'en temps voulut')
+let personne16 = new personnage('4', 'Hug', 'TDMeeting\asset\img\hugoYang.jpg', '29', 'Homme', 'Femme', '75', 'en temps voulut')
+let personne17 = new personnage('5', 'Adiydoy', 'TDMeeting\asset\img\jeanLouisAbdou.jpg', '25', 'Homme', 'Femme', '80', 'en temps voulut')
+let personne18 = new personnage('6', 'doudou', 'TDMeeting\asset\img\jessicaMorvan.jpg', '36', 'Femme', 'homme', '75',' en temps voulut' )
+let personne19 = new personnage('7', 'kiki', 'TDMeeting\asset\img\killianPicard.jpg', '38', 'homme', 'homme', '73',' en temps voulut' )
+let personne20 = new personnage('8', 'Geraud', 'TDMeeting\asset\img\liliGeraud.jpg', '30', 'homme', 'femme', '75',' en temps voulut' )
+let personne21 = new personnage('9', 'Louna', 'TDMeeting\asset\img\LounaDaCosta.jpg', '25', 'Femme', 'homme', '75',' en temps voulut' )
+let personne22 = new personnage('10', 'Yoyo', 'TDMeeting\asset\img\yohannLolo.jpg', '32', 'Homme', 'Femme', '76',' en temps voulut' )
+let personne23 = new personnage('11', 'Sam', 'TDMeeting\asset\img\samiraPerrin.jpg', '24', 'Femme', 'homme', '75',' en temps voulut' )
+let personne24 = new personnage('12', 'mimi', 'TDMeeting\asset\img\marliesRiedl.jpg', '26', 'Femme', 'homme', '73',' en temps voulut' )
+
+
+
+
+
+
+
+
+
+
+
+
+
+let arrayPersonnes = [personne1, personne2, personne3, personne4, personne5, personne6, personne7, personne8, personne9, personne10, personne11, personne12]
